aff2225cad5d119bfe3a8c57dfd034a5d5228641
Documenter.jl <documenter@juliadocs.github.io>
2021-02-10 05:19:17 +0000

    build based on 40e52a3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BenchmarkPlottingUtilities","category":"page"},{"location":"#BenchmarkPlottingUtilities","page":"Home","title":"BenchmarkPlottingUtilities","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BenchmarkPlottingUtilities]","category":"page"}]
+}
